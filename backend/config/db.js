9112b6e0d80bca0ebc959435fce83ef5d0a4dd73
Isaac1th <Isaacadis1th@gmail.com>
2022-12-11 20:34:35 +0200

    add orderModel, productModel, userModel, add seeder.js script

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', true); // Remove deprecation warning
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (err) {
+    console.log(`Error: ${err.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
