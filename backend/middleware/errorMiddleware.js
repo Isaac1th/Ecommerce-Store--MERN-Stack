baaad3d4fb83b1979d9cbb598c5b240a0d352d24
Isaac1th <Isaacadis1th@gmail.com>
2022-12-14 09:19:49 +0200

    add custom error handling with error middlware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,13 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+export { notFound, errorHandler };
