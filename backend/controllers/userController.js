06364688bfeeea21f793d9d6c63934364736912c
Isaac1th <Isaacadis1th@gmail.com>
2022-12-17 14:45:07 +0200

    add cart view component, add carousel component for top rated products, add constants for cart,order, & user, add product & user controllers, add jwt authentication, add authentication middleware

---
+++ backend/controllers/userController.js
@@ -0,0 +1,43 @@
+import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
+import User from '../models/userModel.js';
+
+// Auth user & get token
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+// Get user profile
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,18 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js';
+import {
+  getProductById,
+  getProducts,
+  getTopProducts,
+} from '../controllers/productController.js';
 
-// Fetch all products
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+// Get all products
+router.get('/', getProducts);
 
-    res.json(products);
-  })
-);
+// Get top rated products
+router.get('/top', getTopProducts);
 
-// Fetch product
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+// Get product by id
+router.get('/:id', getProductById);
 
 export default router;
