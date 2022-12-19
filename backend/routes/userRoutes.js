89814d631cd39de5527d155fb320b18cf6a43c23
Isaac1th <Isaacadis1th@gmail.com>
2022-12-19 13:01:01 +0200

    add login & register components, implement login & register logic

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,20 @@
 import express from 'express';
+
 const router = express.Router();
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
+
+router.post('/', registerUser);
 
 router.post('/login', authUser);
 
-router.get('/profile', protect, getUserProfile);
+router
+  .get('/profile', protect, getUserProfile)
+  .put('/profile', protect, updateUserProfile);
 
 export default router;
