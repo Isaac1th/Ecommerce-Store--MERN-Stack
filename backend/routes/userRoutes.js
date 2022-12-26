66cbd118a97cc83e9a90870c8f4befec6327a4da
Isaac1th <Isaacadis1th@gmail.com>
2022-12-26 14:53:33 +0200

    add admin functionalities - edit & update users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,18 +3,27 @@ import express from 'express';
 const router = express.Router();
 import {
   authUser,
+  deleteUser,
   getUserProfile,
+  getUsers,
   registerUser,
   updateUserProfile,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.post('/', registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 
 router.post('/login', authUser);
 
 router
   .get('/profile', protect, getUserProfile)
   .put('/profile', protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
